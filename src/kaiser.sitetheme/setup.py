aa4fa519e6b98a3f6b74a05bde13c5e179ae15ed
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-17 14:49:47 +0100

    Add theme package scaffold

---
+++ src/kaiser.sitetheme/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='kaiser.sitetheme',
+      version=version,
+      description="kaiser Diazo Plone Theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Vorwaerts Werbung GbR',
+      author_email='hallo@vorwaerts-werbung.de',
+      url='http://dist.vorwaerts-werbung.de',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['kaiser'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.theming',
+          'plone.app.themingplugins',
+          'plone.app.toolbar',
+          'plone.app.widgets',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
